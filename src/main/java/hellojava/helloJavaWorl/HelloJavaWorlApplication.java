cc61890ef22e3cd00880d95627315a848d768ccc
Michal <michal.malczyk@gmail.com>
2020-03-03 14:49:43 +0100

    git init

---
+++ src/main/java/hellojava/helloJavaWorl/HelloJavaWorlApplication.java
@@ -0,0 +1,13 @@
+package hellojava.helloJavaWorl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaWorlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJavaWorlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hellojava/helloJavaWorl/HelloJavaWorlApplicationTests.java
@@ -0,0 +1,13 @@
+package hellojava.helloJavaWorl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJavaWorlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
